54c2f9727ac5cfa59da41d1987d3c9c3860d27f7
kusnandi <72843119+kusnandi@users.noreply.github.com>
2020-11-08 04:19:36 +0700

    Add files via upload

---
+++ if1.10119024.Latihan34.objectorientedkalkulator/src/if1/pkg10119024/latihan34/objectorientedkalkulator/Kalkulator.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119024.latihan34.objectorientedkalkulator;
+
+/**
+ *
+ * @author Kusnandi
+ * NAMA  : Kusnandi Pirmansyah
+ * KELAS : IF-1
+ * NIM   : 10119024
+ */
+public class Kalkulator {
+    public double value1, value2;
+    
+    public double tambahBilangan() {
+        return value1 + value2;
+    }
+    
+    public double kurangBilangan() {
+        return value1 - value2;
+    }
+    
+    public double kaliBilangan() {
+        return value1 * value2;
+    }
+    
+    public double bagiBilangan() {
+        return value1 / value2;
+    }
+    
+    public double sisaBilangan() {
+        return value1 % value2;
+    }
+    
+}
